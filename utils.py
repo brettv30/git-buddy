c7acd1167fcfacbddd080d69cea72789d940b091
brettv30 <brettvogelsang30@gmail.com>
2023-12-14 22:56:53 -0500

    Add additional source search functionality
    Added actual documentation searching for each of the sources we expect to pull from the vector DB

--- utils.py
+++ utils.py
@@ -25,7 +25,7 @@ INDEX_NAME = "git-buddy-index"
 MODEL_NAME = "gpt-3.5-turbo"
 PROMPT_TEMPLATE = """You are Git Buddy, a helpful assistant that teaches Git, GitHub, and TortoiseGit to beginners. Your responses are geared towards beginners. 
 You should only ever answer questions about Git, GitHub, or TortoiseGit. Never answer any other questions even if you think you know the correct answer. 
-If possible, please provide example code and always provide the name of the file you pulled an answer from if you pulled an answer from the context.
+If possible, please provide example code to help the beginner learn Git commands. If URL sources are 
 
 If a question is ambiguous please refer to the conversation history to see if that helps in answering the question at the end:
  {chat_history}
@@ -33,9 +33,11 @@ If a question is ambiguous please refer to the conversation history to see if th
 Use the following pieces of context to answer the question at the end: 
 {context}
 
+If there are links in the following sources then you MUST link all of the following sources at the end of your answer to the question. You can just keep the entire link in the output, no need to hyperlink with a different name. Do NOT change the links.
+ {url_sources}
+
 Question: {human_input}
-Answer:
-Source(s):"""
+Answer:"""
 
 # Validate environment variables
 if not OPENAI_API_KEY or not PINECONE_API_KEY:
@@ -57,7 +59,8 @@ memory = ConversationBufferWindowMemory(
     k=3,
 )
 prompt = PromptTemplate(
-    input_variables=["chat_history", "context", "human_input"], template=PROMPT_TEMPLATE
+    input_variables=["chat_history", "context", "human_input", "url_sources"],
+    template=PROMPT_TEMPLATE,
 )
 qa_llm = LLMChain(llm=llm, prompt=prompt, memory=memory, verbose=True)
 
@@ -74,37 +77,65 @@ def get_similar_docs(query: str, k: int = 3, score: bool = False) -> list:
 
 
 def get_sources(docs: str) -> str:
+    sources = []
     # Extracting 'source' from each item's metadata
-    sources = [doc["metadata"]["source"] for doc in docs]
+    for doc in docs:
+        sources.append(doc.metadata["source"])
 
     return sources
+
+
+def get_search_query(sources: list):
+    searches = []
+
+    # Regular expression to extract information between '\' and '.'
+    pattern = r"\\(.*?)\."
+
+    for source in sources:
+        page = re.findall(pattern, source)
+        searches.append(page)
+
+    unique_elements = list(set(element for sublist in searches for element in sublist))
+    return unique_elements
+
+
+def parse_urls(search_results: str):
+    # Regular expression to find URLs
+    pattern = r"https://[^\]]+"
+    urls = re.findall(pattern, search_results)
+
+    return urls
 
 
 def get_answer(query: str) -> str:
     """Generate an answer based on similar documents and the provided query."""
     similar_docs = get_similar_docs(query)
-    print(similar_docs[0].metadata["source"])
-    # Regular expression to extract information between '\' and '.'
-    pattern = r"\\(.*?)\."
-    match = re.search(pattern, similar_docs[0].metadata["source"])
-
-    if match:
-        result = match.group(1)
-        print("Extracted text:", result)
-
-        search_results = search.run(f"{result}")
-        print(search_results)
-    else:
-        print("No match found")
+    sources = get_sources(similar_docs)
+    queries = get_search_query(sources)
+    for query in queries:
+        if "GitHub Docs" in query:
+            search_results = search.run(f"site:https://docs.github.com/en {query}")
+            urls = parse_urls(search_results)
+        elif "progit" in query:
+            search_results = search.run(f"site:https://git-scm.com/docs {query}")
+            urls = parse_urls(search_results)
+        elif "Tortoise" in query:
+            search_results = search.run(
+                f"site:https://tortoisegit.org/support/faq/ {query}"
+            )
+            urls = parse_urls(search_results)
+        else:
+            urls = []
 
     answer = qa_llm.run(
         {
             "context": similar_docs,
             "human_input": query,
             "chat_history": memory.load_memory_variables({}),
+            "url_sources": urls,
         }
     )
     return answer
 
 
-print(get_answer("What are GitHub Issues?"))
+print(get_answer("What is Git?"))
