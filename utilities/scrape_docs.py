57de6964487fa3e4a203c945e6e88439a6d602b8
brettv30 <brettvogelsang30@gmail.com>
2023-12-26 17:54:23 -0500

    needed to add all testing to scrape_docs itself

--- utilities/scrape_docs.py
+++ utilities/scrape_docs.py
@@ -3,6 +3,8 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.docstore.document import Document
 from bs4 import BeautifulSoup as Soup
 import re
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
 
 
 def remove_extra_whitespace(my_str):
@@ -72,3 +74,38 @@ def split_docs(documents, chunk_size=400, chunk_overlap=50):
     )
 
     return text_splitter.split_documents(documents)
+
+
+# List of URLs we want to iterate through and add to documentation
+url_list = [
+    "https://docs.github.com/en/get-started",
+    "https://docs.github.com/en/authentication",
+    "https://docs.github.com/en/repositories",
+    # "https://docs.github.com/en/pull-requests",
+    # "https://docs.github.com/en/copilot",
+    # "https://docs.github.com/en/actions",
+    # "https://docs.github.com/en/pages",
+    # "https://docs.github.com/en/migrations"
+    # "https://docs.github.com/en/code-security",
+    # "https://docs.github.com/en/issues",
+    # "https://docs.github.com/en/search-github",
+    "https://git-scm.com/book/en/v2",
+    # "https://tortoisegit.org/docs/tortoisegitmerge"
+    "https://tortoisegit.org/docs/tortoisegit",
+]
+
+docs = [load_docs(url) for url in url_list]
+
+flattened_list = flatten_list_of_lists(docs)
+
+transformed_doc = clean_docs(flattened_list)
+
+chunked_documents = split_docs(transformed_doc)
+
+retriever = FAISS.from_documents(chunked_documents, OpenAIEmbeddings()).as_retriever()
+
+results = retriever.get_relevant_documents(
+    "What is the difference between Git and GitHub?"
+)
+
+print(results)
