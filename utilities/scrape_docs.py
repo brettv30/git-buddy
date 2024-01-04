cfaec1a7b2422f06d8a24f8a8f7bd287040d21a9
brettv30 <brettvogelsang30@gmail.com>
2024-01-03 20:57:46 -0500

    exchange data pdfs for recursive URL Parsing

--- utilities/scrape_docs.py
+++ utilities/scrape_docs.py
@@ -4,12 +4,19 @@ from langchain.docstore.document import Document
 from bs4 import BeautifulSoup as Soup
 import re
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
+from langchain.vectorstores import Chroma
+from langchain.retrievers import BM25Retriever, EnsembleRetriever
+from langchain.document_loaders import DirectoryLoader
+import streamlit as st
+
+OPENAI_API_KEY = st.secrets["SCRAPE_KEY"]
+embeddings_model = "text-embedding-ada-002"
+directory = "data"
 
 
 def remove_extra_whitespace(my_str):
     # Remove useless page content from GitHub docs
-    string_to_remove1 = "GitHub DocsSkip to main contentGitHub DocsVersion: Free, Pro, & TeamSearch GitHub DocsGet started/Exploring integrationsHomeGet startedQuickstartCreate an accountHello WorldSet up GitGitHub flowBe socialCommunicating on GitHubGitHub glossaryGit cheatsheetLearning resourcesOnboardingGetting started with your GitHub accountGetting started with GitHub TeamGetting started with GitHub Enterprise CloudLearning about GitHubGitHub’s plansGitHub language supportTypes of GitHub accountsAccess permissionsGitHub Advanced SecurityChanges to GitHub plansUsing GitHubConnecting to GitHubFeature previewSupported browsersGitHub MobileAllow network accessConnectivity problemsAccessibilityManage theme settingsKeyboard shortcutsGitHub Command PaletteWriting on GitHubStart writing on GitHubQuickstartAbout writing & formattingBasic formatting syntaxWork with advanced formattingOrganized data with tablesCollapsed sectionsCreate code blocksCreate diagramsMathematical expressionsAuto linked referencesAttaching filesAbout task listsPermanent links to codeUsing keywords in issues and pull requestsWork with saved repliesAbout saved repliesCreating a saved replyEditing a saved replyDeleting a saved replyUsing saved repliesShare content with gistsCreating gistsForking and cloning gistsSaving gists with starsExplore projectsContribute to open sourceContribute to a projectSave repositories with starsFollowing peopleFollowing organizationsGetting started with GitSet your usernameCaching credentialsGit passwordsmacOS Keychain credentialsGit workflowsAbout remote repositoriesManage remote repositoriesAssociate text editorsHandle line endingsIgnoring filesUsing GitAbout GitPush commits to a remoteGet changes from a remoteNon-fast-forward errorSplitting a subfolderAbout Git subtree mergesAbout Git rebaseGit rebaseResolve conflicts after rebaseSpecial characters in namesMaximum push limitSubversionSubversion & Git differencesSupport for Subversion clientsProperties supported by GitHubExploring integrationsAbout using integrationsAbout building integrationsFeatured integrationsGitHub Developer ProgramArchive account and public reposRequest account archiveGitHub Archive programUsing GitHub DocsDocs versionsHover cards"
+    string_to_remove1 = "\nThis book is available in\n  English.\n \n  Full translation available in\n  \nazərbaycan dili,\nбългарски език,\nDeutsch,\nEspañol,\nFrançais,\nΕλληνικά,\n日本語,\n한국어,\nNederlands,\nРусский,\nSlovenščina,\nTagalog,\nУкраїнська\n简体中文,\n\n \n  Partial translations available in\n  \nČeština,\nМакедонски,\nPolski,\nСрпски,\nЎзбекча,\n繁體中文,\n\n \n  Translations started for\n  \nБеларуская,\nفارسی,\nIndonesian,\nItaliano,\nBahasa Melayu,\nPortuguês (Brasil),\nPortuguês (Portugal),\nSvenska,\nTürkçe.\n\n \nThe source of this book is hosted on GitHub.\nPatches, suggestions and comments are welcome.\n"
     string_to_remove2 = "Help and supportDid this doc help you?YesNoPrivacy policyHelp us make these docs great!All GitHub docs are open source. See something that's wrong or unclear? Submit a pull request.Make a contributionLearn how to contributeStill need help?Ask the GitHub communityContact supportLegal© 2023 GitHub, Inc.TermsPrivacyStatusPricingExpert servicesBlog"
 
     interim_string = my_str.replace(string_to_remove1, "").replace(
@@ -31,7 +38,9 @@ def load_docs(url, max_depth=3):
     loader = RecursiveUrlLoader(
         url=url,
         max_depth=max_depth,
-        extractor=lambda x: Soup(x, "html.parser").text,
+        extractor=lambda x: " ".join(
+            [p.get_text() for p in Soup(x, "html.parser").find_all("p")]
+        ),
         exclude_dirs=[
             "https://docs.github.com/en/enterprise-cloud@latest",
             "https://docs.github.com/en/enterprise-server@3.11",
@@ -41,6 +50,11 @@ def load_docs(url, max_depth=3):
             "https://docs.github.com/en/enterprise-server@3.7",
         ],
     )
+    return loader.load()
+
+
+def load_pdfs(directory):
+    loader = DirectoryLoader(directory)
     return loader.load()
 
 
@@ -78,9 +92,9 @@ def split_docs(documents, chunk_size=400, chunk_overlap=50):
 
 # List of URLs we want to iterate through and add to documentation
 url_list = [
-    "https://docs.github.com/en/get-started",
+    # "https://docs.github.com/en/get-started",
     "https://docs.github.com/en/authentication",
-    "https://docs.github.com/en/repositories",
+    # "https://docs.github.com/en/repositories",
     # "https://docs.github.com/en/pull-requests",
     # "https://docs.github.com/en/copilot",
     # "https://docs.github.com/en/actions",
@@ -89,22 +103,35 @@ url_list = [
     # "https://docs.github.com/en/code-security",
     # "https://docs.github.com/en/issues",
     # "https://docs.github.com/en/search-github",
-    "https://git-scm.com/book/en/v2",
-    # "https://tortoisegit.org/docs/tortoisegitmerge"
-    "https://tortoisegit.org/docs/tortoisegit",
+    # "https://git-scm.com/book/en/v2",
 ]
 
 docs = [load_docs(url) for url in url_list]
 
+pdfs = load_pdfs(directory)
+
 flattened_list = flatten_list_of_lists(docs)
 
-transformed_doc = clean_docs(flattened_list)
+full_list = flattened_list + pdfs
+
+transformed_doc = clean_docs(full_list)
 
 chunked_documents = split_docs(transformed_doc)
 
-retriever = FAISS.from_documents(chunked_documents, OpenAIEmbeddings()).as_retriever()
+# initialize the bm25 retriever and chroma retriever
+bm25_retriever = BM25Retriever.from_texts(chunked_documents)
+bm25_retriever.k = 5
 
-results = retriever.get_relevant_documents(
+chroma_retriever = Chroma.from_documents(
+    chunked_documents, OpenAIEmbeddings(model=embeddings_model)
+).as_retriever(search_kwargs={"k": 5})
+
+# initialize the ensemble retriever
+ensemble_retriever = EnsembleRetriever(
+    retrievers=[bm25_retriever, chroma_retriever], weights=[0.5, 0.5]
+)
+
+results = ensemble_retriever.get_relevant_documents(
     "What is the difference between Git and GitHub?"
 )
 
