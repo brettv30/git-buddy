26bbe4a52c774e5e28bd5e099d43b92d04bbc5ba
brettv30 <brettvogelsang30@gmail.com>
2024-01-10 22:55:53 -0500

    Update home messaging

--- Home.py
+++ Home.py
@@ -18,7 +18,7 @@ with col3:
 
 st.markdown(
     """
-[Git Buddy](https://git-buddy.streamlit.app/Git_Buddy_Chat_Bot) originated as an idea to utilize a Large Language Model (LLM) as a chatbot tool to assist with teaching others, specifically beginners, about Git, GitHub, and TortoiseGit. Git Buddy's main purpose is to provide a comfortable environment where people starting to learn version control are safe to ask any question that comes to mind. If you're not sure where to start or what to ask, please see `Additional Resources - Starter Questions` for some exmaples.
+[Git Buddy](https://git-buddy.streamlit.app/Git_Buddy_Chat_Bot) originated as an idea to utilize a Large Language Model (LLM) as a chatbot tool to assist with teaching others about Git, GitHub, and TortoiseGit. Git Buddy's main purpose is to provide a comfortable environment where people starting to learn version control can ask any question that comes to mind. If you're not sure where to start or what to ask, please see `Additional Resources - Starter Questions` for some exmaples.
 
 Git Buddy utilizes the following tools:
 
@@ -27,8 +27,8 @@ Git Buddy utilizes the following tools:
 - [OpenAI](https://openai.com) to supply the LLM (GPT-3.5-Turbo)
 - [Streamlit](https://streamlit.io) to deliver the application
 
-Git Buddy follows a Retrieval Augmented Generation (RAG) framework which allows for additional context to be provided to the LLM before passing the entire prompt to the model. The documents stored for retrieval can be found in `Additional Resources - Documentation`. RAG is essential for enhancing LLMs in app development. It supplements LLMs with external data sources, helping arrive at more relevant responses by reducing errors or hallucinations. RAG determines what info is relevant to the user's query through semantic search, which searches data by meaning (rather than just looking for literal matches of search terms). RAG is particularly effective for LLM apps that need to access domain-specific or proprietary data. 
+Git Buddy follows a Retrieval Augmented Generation (RAG) with Re-Rank framework which allows for additional relevant context to be included in a prompt before passing the prompt to the LLM. The documents stored for retrieval can be found in `Additional Resources - Documentation`. RAG is essential for enhancing LLMs in app development. It supplements LLMs with external data sources, helping arrive at more relevant responses by reducing errors or hallucinations. RAG determines what info is relevant to the user's query through semantic search, which searches data by meaning (rather than just looking for literal matches of search terms). RAG is particularly effective for LLM apps that need to access domain-specific or proprietary data. 
 
-Additionally, once similar documents are retrieved from Pinecone they are passed through a web search via [DuckDuckGo](https://python.langchain.com/docs/integrations/tools/ddg) to provide additional website sources in the LLM response. Empowering users not only with new knowledge via the LLM response, but also with actual website links that contain valuable information related to the user's query. This step further reduces errors and hallucinations related to incorrect and inaccurate website links in the LLM response.
+Additionally, once relevant documents are retrieved from Pinecone they are re-ranked using the [Cohere Rerank](https://docs.cohere.com/docs/reranking) API endpoint to allow for additional documents to be extracted from Pinecone and for the most relevent of these documents to pass as LLM context. This step further reduces errors and hallucinations related to incorrect and inaccurate documents used in the LLM prompt.
 """
 )
