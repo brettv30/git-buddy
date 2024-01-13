98f567779fb4fc4692b706d9f3a78c4a92b8caf7
brettv30 <brettvogelsang30@gmail.com>
2024-01-12 22:46:00 -0500

    Imported classes from Utils
    - Converted all function calls to class methods

--- utilities/initialize_index.py
+++ utilities/initialize_index.py
@@ -1,17 +1,15 @@
-# Build the Knowledge Base
 import time
 import pinecone
-import streamlit as st
 from langchain_community.vectorstores import Pinecone
 from langchain_openai import OpenAIEmbeddings
-from utils import load_docs, load_pdfs, flatten_list_of_lists, clean_docs, split_docs
+from utils import DocumentManager, Config
 
-directory = "data"
-index_name = "git-buddy-index"
-embeddings_model = "text-embedding-ada-002"
+# Initialize the configuration componenets
+config = Config()
+doc_manager = DocumentManager(config)
 
-openai_api_key_env = st.secrets["OPENAI_API_KEY"]
-pinecone_api_key_env = st.secrets["PINECONE_API_KEY"]
+openai_api_key_env = config.openai_api_key
+pinecone_api_key_env = config.pinecone_api_key
 
 # List of URLs we want to iterate through and add to documentation
 url_list = [
@@ -20,46 +18,48 @@ url_list = [
 ]
 
 # Load in knowledge base
-docs = [load_docs(url) for url in url_list]
+docs = [doc_manager.load_docs(url) for url in url_list]
 
-pdfs = load_pdfs(directory)
+pdfs = doc_manager.load_pdfs()
 
 # Flatten the list of lists into just a single list
-flattened_list = flatten_list_of_lists(docs)
+flattened_list = [element for sublist in docs for element in sublist]
 
 full_list = flattened_list + pdfs
 
 # Clean the documents/pdfs
-transformed_doc = clean_docs(full_list)
+transformed_doc = doc_manager.clean_docs(full_list)
 
 # Create docs to pass to langchain
-chunked_documents = split_docs(transformed_doc)
+chunked_documents = doc_manager.split_docs(transformed_doc)
 
 # Initialize the Pinecone Vector Database
 pinecone.init(environment="gcp-starter")  # next to api key in console
 
-if index_name in pinecone.list_indexes():
-    pinecone.delete_index(index_name)
+if config.index_name in pinecone.list_indexes():
+    pinecone.delete_index(config.index_name)
 
 # we create a new index
 pinecone.create_index(
-    name=index_name,
+    name=config.index_name,
     metric="dotproduct",  # https://www.pinecone.io/learn/vector-similarity/
     dimension=1536,  # 1536 dim of text-embedding-ada-002
 )
 
 # wait for index to be initialized
-while not pinecone.describe_index(index_name).status["ready"]:
+while not pinecone.describe_index(config.index_name).status["ready"]:
     time.sleep(1)
 
 # Initialize the Pinecone Vector Database
 pinecone.init(environment="gcp-starter")  # next to api key in console
 
-# if you already have an index, you can load it like this
-embeddings = OpenAIEmbeddings(model=embeddings_model)
+# if you already have an index, you can load it using the steps below
+embeddings = OpenAIEmbeddings(model=config.embeddings_model)
 
 
-# This actually loads the data/embeddings into your index
-index = Pinecone.from_documents(chunked_documents, embeddings, index_name=index_name)
+# This loads the embedded documents into your Pinecone Index
+index = Pinecone.from_documents(
+    chunked_documents, embeddings, index_name=config.index_name
+)
 
 print("Index Creation Complete!")
